55517a9eb614c78d7bf6e39df0b40fc143948552
nikhil <nikhiltharlada310@gmail.com>
2026-06-24 23:11:32 +0530

    debug upload

--- shared_data/supabase_client.py
+++ shared_data/supabase_client.py
@@ -7,9 +7,14 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_SECRET_KEY")
 
+print("SUPABASE_URL =", SUPABASE_URL)
+print("SUPABASE_KEY EXISTS =", SUPABASE_KEY is not None)
+
 supabase = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
 )
 
 BUCKET = os.getenv("BUCKET_NAME")
+
+print("BUCKET =", BUCKET)
